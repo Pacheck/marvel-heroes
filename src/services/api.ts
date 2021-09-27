8a0ee7d8e316928517aaf07408aa9e2334155d84
Pedro Pacheco <pacheckdev@gmail.com>
2021-09-27 13:39:01 -0300

    update request limit to 100

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,6 +5,7 @@ const apiKey = '19c20d40869ff15779b14dcbc52d2895';
 export default axios.create({
   baseURL: 'https://gateway.marvel.com:443/v1/public/',
   params: {
+    limit: 100,
     apikey: apiKey,
   },
 });
