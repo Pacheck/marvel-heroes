115951fa93bff7abff13aa683c790f18b693b30a
Pedro Pacheco <pacheckdev@gmail.com>
2021-09-27 13:38:22 -0300

    finished carousel card logic

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -25,6 +25,10 @@ const Home = () => {
   const dispatch = useDispatch();
   const characterStore = useSelector((state: any) => state.characters);
   const [cardPosition, setCardPosition] = useState([0, 3]);
+  const [disableArrows, setDisableArrows] = useState({
+    backwardArrow: true,
+    forwardArrow: false,
+  });
 
   const whichCardsRender = (item: CharacterProp, index: number) => {
     if (index >= cardPosition[0] && index <= cardPosition[1]) {
@@ -36,12 +40,14 @@ const Home = () => {
   const handleNextCards = () => {
     if (cardPosition[1] < characterStore.characters.length - 1) {
       setCardPosition([cardPosition[0] + 3, cardPosition[1] + 3]);
+      setDisableArrows({ ...disableArrows, backwardArrow: false });
     }
   };
 
   const handlePreviousCards = () => {
     if (cardPosition[0] > 0) {
       setCardPosition([cardPosition[0] - 3, cardPosition[1] - 3]);
+      setDisableArrows({ ...disableArrows, forwardArrow: false });
     }
   };
 
@@ -50,6 +56,15 @@ const Home = () => {
       dispatch(getAllCharacters());
     }
   }, []);
+
+  useEffect(() => {
+    if (cardPosition[0] - 1 < 0) {
+      setDisableArrows({ ...disableArrows, backwardArrow: true });
+    }
+    if (cardPosition[1] + 1 > 100) {
+      setDisableArrows({ ...disableArrows, forwardArrow: true });
+    }
+  }, [cardPosition]);
 
   return (
     <S.Container>
@@ -75,9 +90,13 @@ const Home = () => {
           </S.SubText>
         </S.TextContainer>
         <S.CarrouselContainer>
-          <BackwardArrow handleClick={handlePreviousCards} />
+          {!disableArrows.backwardArrow && (
+            <BackwardArrow handleClick={handlePreviousCards} />
+          )}
           {characterStore.characters.map(whichCardsRender)}
-          <ForwardArrow handleClick={handleNextCards} />
+          {!disableArrows.forwardArrow && (
+            <ForwardArrow handleClick={handleNextCards} />
+          )}
         </S.CarrouselContainer>
       </S.MainContent>
     </S.Container>
