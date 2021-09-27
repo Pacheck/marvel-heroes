163a1beb51566049bba95d7796a8d21cf68bfaac
Pedro Pacheco <pacheckdev@gmail.com>
2021-09-27 13:07:24 -0300

    update getchar detail action, update reducer

--- src/redux/actions/character.actions.ts
+++ src/redux/actions/character.actions.ts
@@ -4,20 +4,34 @@ import { Dispatch } from 'redux';
 const GET_ALL_CHARACTERS = 'GET_ALL_CHARACTERS';
 const GET_CHARACTER_DETAIL = 'GET_CHARACTER_DETAIL';
 
+const GET_ALL_CHARACTERS_ERROR = 'GET_ALL_CHARACTERS_ERROR';
+const GET_CHARACTER_DETAIL_ERROR = 'GET_CHARACTER_DETAIL_ERROR';
+const SET_CHARACTER_DETAIL = 'SET_CHARACTER_DETAIL';
+const CLEAR_CHARACTER_DETAIL = 'CLEAR_CHARACTER_DETAIL';
+
 export const getAllCharacters = () => async (dispatch: Dispatch) => {
-  const characters = await api.get('characters');
-  console.log(characters);
-  dispatch({ type: GET_ALL_CHARACTERS, payload: characters.data.data.results });
+  try {
+    const characters = await api.get('characters');
+    console.log(characters);
+    dispatch({
+      type: GET_ALL_CHARACTERS,
+      payload: characters.data.data.results,
+    });
+  } catch (err) {
+    // dispatch({ type: GET_ALL_CHARACTERS_ERROR, payload: err });
+  }
 };
 
 export const getCharacterDetails =
-  (characterID: number) => async (dispatch: Dispatch) => {
-    try {
-      // 1017100
-      const characterDetail = await api.get(`characters/${characterID}`);
-      //   dispatch({ type: GET_CHARACTER_DETAIL, payload:  });
-      console.log(characterDetail.data.data.results[0]);
-    } catch (error) {
-      console.log(error);
-    }
+  (characterID: number | string) => (dispatch: Dispatch) => {
+    console.log('action', characterID);
+    dispatch({ type: GET_CHARACTER_DETAIL, payload: { id: characterID } });
   };
+
+export const setCharacterDetails = (character: any) => (dispatch: Dispatch) => {
+  dispatch({ type: SET_CHARACTER_DETAIL, payload: character });
+};
+
+export const clearDetailedCharacter = (dispatch: Dispatch) => {
+  dispatch({ type: CLEAR_CHARACTER_DETAIL });
+};
--- src/redux/reducers/character.reducer.ts
+++ src/redux/reducers/character.reducer.ts
@@ -1,14 +1,49 @@
-const initialState = {
+import { CharacterProp } from '../../types';
+
+const initialState: {
+  characters: CharacterProp[];
+  character: CharacterProp | null;
+  errors: string[];
+} = {
   characters: [],
-  character: {},
+  character: null,
+  errors: [],
 };
+
+const GET_ALL_CHARACTERS = 'GET_ALL_CHARACTERS';
+const GET_CHARACTER_DETAIL = 'GET_CHARACTER_DETAIL';
+const GET_ALL_CHARACTERS_ERROR = 'GET_ALL_CHARACTERS_ERROR';
+const GET_CHARACTER_DETAIL_ERROR = 'GET_CHARACTER_DETAIL_ERROR';
+const CLEAR_CHARACTER_DETAIL = 'CLEAR_CHARACTER_DETAIL';
 const CharacterReducer = (state = initialState, action: any) => {
   switch (action.type) {
-    case 'GET_ALL_CHARACTERS':
-      console.log(action.payload);
+    case CLEAR_CHARACTER_DETAIL:
+      return { ...state, character: null };
+    case GET_ALL_CHARACTERS:
       return { ...state, characters: action.payload };
-    case 'GET_CHARACTER_DETAIL':
-      return { ...state, character: action.payload };
+    case GET_CHARACTER_DETAIL:
+      const { id } = action.payload;
+      const character = state.characters.find((char) => id === char.id);
+      return { ...state, character };
+    case 'SET_CHARACTER_DETAIL':
+      const oldState = state.characters;
+      const oldChar = state.characters.find(
+        (char) => char.id === action.payload.id
+      );
+      const oldCharIndex = state.characters.findIndex(
+        (char) => char.id === action.payload.id
+      );
+      const newCharProps = {
+        ...oldChar,
+        ...action.payload,
+      };
+      oldState[oldCharIndex] = newCharProps;
+      console.log({ newCharProps });
+      return { ...state, characters: oldState };
+    case GET_CHARACTER_DETAIL_ERROR:
+      return { ...state, errors: [action.payload] };
+    case GET_ALL_CHARACTERS_ERROR:
+      return { ...state, errors: [action.payload] };
     default:
       return state;
   }
